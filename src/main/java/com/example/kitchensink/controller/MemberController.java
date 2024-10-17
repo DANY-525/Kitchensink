0f8c8fc8df1b14c8d1d0ad8bf780947036fb9fc3
Daniel Saavedra <dany525@gmail.com>
2024-10-16 22:25:24 -0500

    firts changes

--- src/main/java/com/example/kitchensink/controller/MemberController.java
+++ src/main/java/com/example/kitchensink/controller/MemberController.java
@@ -1,22 +1,18 @@
 package com.example.kitchensink.controller;
 
+import com.example.kitchensink.model.Member;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @CrossOrigin(origins = "*")
-@RestController
-
+@RequestMapping
 public class MemberController {
+
     @PostMapping
-    public ResponseEntity<Member> addMember (@RequestBody Member member){
-        System.out.println(member);
-        return null;
+    public ResponseEntity<?> addMember(@RequestBody Member member){
+          return new  ResponseEntity<>(member, HttpStatus.OK);
     }
-
-
-
 
 }
