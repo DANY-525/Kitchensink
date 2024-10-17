89fe7287b467f41276af61d990f39d1d6b7e5420
Daniel Saavedra <dany525@gmail.com>
2024-10-16 22:25:17 -0500

    firts changes

---
+++ src/main/java/com/example/kitchensink/model/Member.java
@@ -0,0 +1,14 @@
+package com.example.kitchensink.model;
+
+import lombok.Data;
+
+@Data
+
+public class Member {
+
+    private String name;
+
+    private String email;
+
+    private String phoneNumber;
+}
